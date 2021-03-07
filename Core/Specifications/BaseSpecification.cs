0c532c130aa215f0ba9456e94bf410d008342984
lannhi02 <phamtrinhlannhi02@gmail.com>
2021-03-08 00:41:50 +0700

    paging

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -15,14 +15,20 @@ namespace Core.Specifications
             Criteria = criteria;
         }
 
-        public Expression<Func<T, bool>> Criteria {get; }
+        public Expression<Func<T, bool>> Criteria { get; }
 
-        public List<Expression<Func<T, object>>> Includes {get; } = 
+        public List<Expression<Func<T, object>>> Includes { get; } =
             new List<Expression<Func<T, object>>>();
 
-        public Expression<Func<T, object>> OrderBy {get; private set; }
+        public Expression<Func<T, object>> OrderBy { get; private set; }
 
-        public Expression<Func<T, object>> OrderByDescending {get; private set; }
+        public Expression<Func<T, object>> OrderByDescending { get; private set; }
+
+        public int Take { get; private set; }
+
+        public int Skip { get; private set; }
+
+        public bool IsPagingEnabled { get; private set; }
 
         protected void AddInclude(Expression<Func<T, object>> includeExpression)
         {
@@ -33,9 +39,17 @@ namespace Core.Specifications
         {
             OrderBy = orderByExpression;
         }
+
         protected void AddOrderByDescending(Expression<Func<T, object>> orderByDesExpression)
         {
             OrderByDescending = orderByDesExpression;
+        }
+
+        protected void ApplyPaging(int skip, int take)
+        {
+            Skip = skip;
+            Take = take;
+            IsPagingEnabled = true;
         }
     }
 }
