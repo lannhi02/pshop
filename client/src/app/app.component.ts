a69056bf1dce3390a12a994258234ef6a83a13fc
lannhi02 <phamtrinhlannhi02@gmail.com>
2021-03-09 18:35:26 +0700

    http client module

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pShop';
+  products: any[];
+
+  constructor(private http: HttpClient)
+  {
+
+  }
+
+  ngOnInit(): void {
+    this.http.get("https://localhost:5001/api/products?pageSize=20").subscribe((response: any) => {
+      console.log(response);
+      this.products = response.data;
+    }, error => {
+      console.log(error);
+    });
+  }
 }
