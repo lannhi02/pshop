b10c959b39af4827b9b43676e7bebaaaf675d91a
lannhi02 <phamtrinhlannhi02@gmail.com>
2021-03-10 08:26:03 +0700

    brand & type selected filter

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,6 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IBrand } from '../shared/models/brand';
 import { IPagination } from '../shared/models/pagination';
+import { IType } from '../shared/models/productType';
+import {delay, map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +13,31 @@ export class ShopService {
 
   constructor(private http: HttpClient) { }
 
-  getProducts() {
-    return this.http.get<IPagination>(this.baseUrl + 'products?pageSize=20');
+  getProducts(brandId?: number, typeId?: number){
+    let params = new HttpParams();
+
+    if(brandId) {
+      params = params.append('brandId', brandId.toString());
+    }
+
+    if(typeId) {
+      params = params.append('typeId', typeId.toString());
+    }
+
+    return this.http.get<IPagination>(this.baseUrl + 'products', {observe: 'response', params})
+    .pipe(
+      delay(100),
+      map(response => {
+        return response.body;
+      })
+    );
+  }
+
+  getBrands() {
+    return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
+  }
+
+  getTypes() {
+    return this.http.get<IType[]>(this.baseUrl + 'products/types');
   }
 }
