bf94aed0b6cb5bf87ccda2bb41c5bce5935c446f
lannhi02 <phamtrinhlannhi02@gmail.com>
2021-03-14 15:56:47 +0700

    xng-breadcrumb

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,12 +6,13 @@ import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'test-error', component: TestErrorComponent},
-  {path: 'server-error', component: ServerErrorComponent},
-  {path: 'not-found', component: NotFoundComponent},
-  {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule)},
-  {path: '**', redirectTo: '', pathMatch: 'full'}
+  {path: '', component: HomeComponent, data: {breadcrumb: 'Trang chủ'}},
+  {path: 'test-error', component: TestErrorComponent, data: {breadcrumb: 'Lỗi'}},
+  {path: 'server-error', component: ServerErrorComponent, data: {breadcrumb: 'Server Errors'}},
+  {path: 'not-found', component: NotFoundComponent, data: {breadcrumb: 'Not Found'}},
+  {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule),
+      data: {breadcrumb: 'Sản phẩm'}},
+  {path: '**', redirectTo: 'not-found', pathMatch: 'full'}
 ];
 
 @NgModule({
