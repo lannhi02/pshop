5e89ca153029268e7dc7c67cca1c42653969aed1
lannhi02 <phamtrinhlannhi02@gmail.com>
2021-03-17 17:46:31 +0700

    localStorage basket

---
+++ client/src/app/shared/models/basket.ts
@@ -0,0 +1,21 @@
+import { v4 as uuid } from 'uuid';
+
+export interface IBasket {
+    id: string;
+    items: IBasketItem[];
+  }
+  
+export interface IBasketItem {
+    id: number;
+    productName: string;
+    price: number;
+    quantity: number;
+    pictureUrl: string;
+    brand: string;
+    type: string;
+  }
+
+export class Basket implements IBasket {
+    id: string = uuid();
+    items: IBasketItem[] = [];   
+} 
