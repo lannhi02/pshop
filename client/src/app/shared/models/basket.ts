154142e20e9a71075c3c84f609e78d9f2dc9e274
lannhi02 <phamtrinhlannhi02@gmail.com>
2021-03-17 19:55:42 +0700

    basket service

--- client/src/app/shared/models/basket.ts
+++ client/src/app/shared/models/basket.ts
@@ -19,3 +19,9 @@ export class Basket implements IBasket {
     id: string = uuid();
     items: IBasketItem[] = [];   
 } 
+
+export interface IBasketTotals {
+  shipping: number;
+  subtotal: number;
+  total: number;
+}
