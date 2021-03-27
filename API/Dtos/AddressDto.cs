57b42e286d1a49d9a5fca0e85211a06c30ca5865
lannhi02 <phamtrinhlannhi02@gmail.com>
2021-03-27 21:19:36 +0700

    user address

---
+++ API/Dtos/AddressDto.cs
@@ -0,0 +1,9 @@
+namespace API.Dtos
+{
+    public class AddressDto
+    {        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Street { get; set; }
+    }
+}
