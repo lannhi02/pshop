53fc90796a6d883dc9a394bc6e5ebc1e0a969c86
lannhi02 <phamtrinhlannhi02@gmail.com>
2021-03-28 11:15:43 +0700

    user login

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './account/account.service';
 import { BasketService } from './basket/basket.service';
 
 @Component({
@@ -9,15 +10,31 @@ import { BasketService } from './basket/basket.service';
 export class AppComponent implements OnInit {
   title = 'pShop';
 
-  constructor(private basketService: BasketService) {
+  constructor(private basketService: BasketService, private accountService: AccountService) {
 
   }
 
   ngOnInit(): void {    
+    this.loadBasket();
+    this.loadCurrentUser();
+  }
+
+  loadCurrentUser() {
+    const token = localStorage.getItem('token');
+    if(token) {
+      this.accountService.loadCurrentUser(token).subscribe(() => {
+        console.log('Đã có user đăng nhập');
+      }, error => {
+        console.log(error);
+      })
+    }
+  }
+
+  loadBasket() {
     const basketId = localStorage.getItem('basket_id');
     if (basketId) {
       this.basketService.getBasket(basketId).subscribe(() => {
-        console.log('Giỏ hàng đã tạo');
+        console.log('Đã có giỏ hàng');
       }, error => {
         console.log(error);
       });
