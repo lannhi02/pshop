18efc34eea18363d44a61bdfb2d306b09e749bfd
lannhi02 <phamtrinhlannhi02@gmail.com>
2021-04-01 21:10:37 +0700

    orders api done

--- API/Controllers/OrdersController.cs
+++ API/Controllers/OrdersController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using API.Dtos;
 using API.Errors;
@@ -33,6 +34,34 @@ namespace API.Controllers
             if(order == null) return BadRequest(new ApiReponse(400, "Đã có lỗi khi tạo đơn hàng"));
 
             return Ok(order);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<OrderDto>>> GetOrdersForUser()
+        {
+            var email = HttpContext.User.RetriveEmailFromPrincipal();
+
+            var orders = await _orderService.GetOrdersForUserAsync(email);
+
+            return Ok(_mapper.Map<IReadOnlyList<Order>, IReadOnlyList<OrderToReturnDto>>(orders));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OrderToReturnDto>> GetOrderByIdForUser(int id)
+        {
+            var email = HttpContext.User.RetriveEmailFromPrincipal();
+
+            var order = await _orderService.GetOrderByIdAsync(id, email);
+
+            if(order == null) return NotFound(new ApiReponse(404));
+
+            return _mapper.Map<Order, OrderToReturnDto>(order);
+        }
+
+        [HttpGet("deliveryMethods")]
+        public async Task<ActionResult<IReadOnlyList<DeliveryMethod>>> GetDeliveryMethods() 
+        {
+            return Ok(await _orderService.GetDeliveryMethodsAsync());
         }
     }
 }
